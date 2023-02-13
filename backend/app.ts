42dd7f233d2b326727b43c818daa5443f0b0c444
Tomáš Porwolik <tomas.porwolik@vacuumlabs.com>
2023-02-13 17:03:23 +0100

    Allow cors

--- backend/app.ts
+++ backend/app.ts
@@ -2,10 +2,12 @@ import dotenv from "dotenv";
 import express from "express";
 import convertRouter from "./routes/convert";
 import currenciesRouter from "./routes/currencies";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.head("/", (_req, res) => {
   res.status(200);
